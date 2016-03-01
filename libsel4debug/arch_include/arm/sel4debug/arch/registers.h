26c0398e42ba3aa972cb61659a6199d2094cff20
Adrian Danis <Adrian.Danis@nicta.com.au>
2016-03-01 12:18:10 +1100

    libsel4debug: Define x86 registers by sel4_arch instead of arch

--- libsel4debug/arch_include/arm/sel4debug/arch/registers.h
+++ libsel4debug/arch_include/arm/sel4debug/arch/registers.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2014, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
 
 #ifndef __DEBUG_ARCH_REGISTERS_H
 #define __DEBUG_ARCH_REGISTERS_H
@@ -11,18 +20,18 @@ static UNUSED const char *register_names[] = {
     "sp",
     "cpsr",
     "r0",
-    "r1", 
+    "r1",
     "r8",
     "r9",
-    "r10", 
+    "r10",
     "r11",
     "r12",
-    "r2", 
+    "r2",
     "r3",
     "r4",
-    "r5", 
-    "r6", 
-    "r7", 
+    "r5",
+    "r6",
+    "r7",
     "lr"
 };
 
